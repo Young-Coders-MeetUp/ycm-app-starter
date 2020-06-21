a5d99c0754a4f134a7d67eb8c03ac75018bec309
malaix <malaika@malaix.com>
2020-06-21 20:59:44 +0100

    Update index.js
    New buttons.

--- App/screens/About/index.js
+++ App/screens/About/index.js
@@ -1,8 +1,12 @@
 import * as React from "react";
-import { Platform, StyleSheet, Text, View, ScrollView } from "react-native";
+import { Platform, StyleSheet, Text, View, ScrollView, Button } from "react-native";
 import ScreenHeader from "../../components/ScreenHeader";
 import Logo from "../../components/Logo";
+
 import * as Linking from 'expo-linking';
+const handlePress = (url) => {
+  Linking.openURL(url);
+};
 export default function About() {
   return (
     <View>
@@ -29,9 +33,15 @@ export default function About() {
           <Logo />
         </View>
         <Text style={styles.paragraph}>
-          This app, created be the YCM (Young Coder's Meetup), is designed to help Deaf learners take more responsibility of their own education. We have also entered the app into the Longitude Explorer Prize and hope to win. Here is the link to the website:</Text> 
-          Linking.openURL('https://longitudeexplorer.challenges.org/im-a-longitude-explorer-finalist/')
-          <Text>We hope you enjoy the app! Stay in touch:</Text>
+          This app, created be the YCM (Young Coder's Meetup), is designed to help Deaf learners 
+          take more responsibility of their own education. We have also entered the app into the Longitude Explorer Prize and hope to win! </Text>
+          <Button onPress ={()=>handlePress('https://longitudeexplorer.challenges.org/im-a-longitude-explorer-finalist/')} title='Longitude Explorer Prize'></Button>
+          <Text></Text>
+          <Button onPress ={()=>handlePress('https://youngcodersmeetup3.wixsite.com/website')} title='Sign2Word project website.'></Button>
+          <Text></Text>
+          <Button onPress ={()=>handlePress('https://youngcodersmeetup.wixsite.com/ycm-uk')} title='YCM website'></Button>
+      
+          <Text style={styles.paragraph}>We hope you enjoy the app! </Text>
         
       </View>
       </ScrollView>
